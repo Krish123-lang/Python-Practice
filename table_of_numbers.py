02b4c1f968d51e4c9f7c9ab122559145ca5eed81
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2023-01-17 19:55:41 +0545

    Create table_of_numbers.py

---
+++ table_of_numbers.py
@@ -0,0 +1,10 @@
+# To Print the table of the number.
+
+a = int(input("Enter the number you want to print table of: "))
+b = int(input("Enter the number you want upto which you want print table: "))
+
+for i in range(1, b+1):
+  print(f"{a} x {i} = ",i*a)
+  
+  
+ 
