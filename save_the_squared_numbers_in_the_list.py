7eac9173c6ab4a430c62dae5c673859f6a7e94ee
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2023-01-18 09:04:30 +0545

    Update save_the_squared_numbers_in_the_list.py

--- save_the_squared_numbers_in_the_list.py
+++ save_the_squared_numbers_in_the_list.py
@@ -1,6 +1,18 @@
 # Print the square of the number from 1 to 10 and then store it in the list.
 
+###########PREVIOUS CODE ################
+# a = []                                #
+# for i in range(1, 11):                #
+#   a.append(i**2)                      #
+# print(a)                              #
+###########PREVIOUS CODE ################
+
+
 a = []
-for i in range(1, 11):
+
+x = int(input("Enter the first number: "))
+y = int(input("Enter the second number: "))
+
+for i in range(x, y+1):
   a.append(i**2)
 print(a)
