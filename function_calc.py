8b75688f3b1afc3c9c2b1795caea6553a3883013
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2023-02-07 09:00:21 +0545

    Create function_calc.py

---
+++ function_calc.py
@@ -0,0 +1,40 @@
+# Function Calculator
+
+def func_Calc():
+    try:
+        while True:
+            a = int(input("Enter first number: "))
+            b = int(input("Enter second number: "))
+
+            print("""
+            *** Enter your choice ***
+            +, -, * /
+            """)
+
+            choice = input("Enter your choice: ")
+
+            if choice == "+":
+                # print(f"The addition of {a} and {b} is {a+b}")
+                return f"The addition of {a} and {b} is {a+b}"
+
+            elif choice == "-":
+                # print(f"The subtraction of {a} and {b} is {a-b}")
+                return f"The subtraction of {a} and {b} is {a-b}"
+
+            elif choice == "*":
+                # print(f"The multiplication of {a} and {b} is {a*b}")
+                return f"The multiplication of {a} and {b} is {a*b}"
+
+            elif choice == "/":
+                # print(f"The divison of {a} and {b} is {a/b}")
+                return f"The divison of {a} and {b} is {a/b}"
+
+            else:
+                print("Invalid Choice !!!")
+                break
+    except Exception as e:
+        print(e)
+
+
+result = func_Calc()
+print(result)
